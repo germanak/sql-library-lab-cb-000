bb45ae384c78e50d48f545426caea0da86afa1ff
Alec German <alec_german@yahoo.com>
2018-03-14 01:46:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN characters
   ON characters.series_id = series.id
-  ORDER BY MAX(characters.specie)"
+  ORDER BY MAX(characters.species)"
 end
 
 def select_character_names_and_number_of_books_they_are_in
