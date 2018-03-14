5cee802e9047a80553af6d6ef28a1d71e7750b44
Alec German <alec_german@yahoo.com>
2018-03-14 01:23:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   JOIN characters
-  ON characters.series_id = series.id
+  ON characters.series_id = series.id WHERE characters.species = 'human'
   ORDER BY characters.species"
 end
 
